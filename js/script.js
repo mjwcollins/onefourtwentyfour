3393b2a147e1be289cb4c4283a8378b80db1d069
mjwcollins <markjwcollins@gmail.com>
2019-05-06 16:30:43 -0700

    project week d1.1

--- js/script.js
+++ js/script.js
@@ -1,26 +1,104 @@
 /*----- constants -----*/ 
-const DICE = {
-    '1': 'one',
-    '2': 'two',
-    '3': 'three',
-    '4': 'four',
-    '5': 'five',
-    '6': 'six'
+// make an array of objects
+
+const dice = [
+    {
+        name: 'one',
+        img: '';
+        points 1
+    }
+]
+
+//array.find
+
+//const foundDie = dice.find(die) => {
+    return dice.points === diceIdx
+}
+const diceIdx = Math.floor(Math.random() * 6) + 1
+
+const dice = {
+    one: {
+        img: 'https://i.imgur.com/U8Aweq3.jpg',
+        points: 1
+    },
+    two: {
+        img: 'https://i.imgur.com/3Sl88UA.jpg',
+        points: 2
+    },
+    three: {
+        img: 'https://i.imgur.com/C8WATr2.jpg',
+        points: 3
+    },
+    four: {
+        img: 'https://i.imgur.com/GF6f5l3.jpg',
+        points: 4
+    },
+    five: {
+        img: 'https://i.imgur.com/IFvJmjc.jpg',
+        points: 5
+    },
+    six: {
+        img: 'https://i.imgur.com/umF86qj.jpg',
+        points: 6
+    }
 };
 
 /*----- app's state (variables) -----*/ 
-var board, playerOneScore, playerTwoScore;
+var results, scores, winner, lScore = 0, rScore = 0, tieScore = 0;
 
 /*----- cached element references -----*/ 
+const lScoreEl = document.querySelector('#left > h1');
+const rScoreEl = document.querySelector('#right > h1');
+const tieScoreEl = document.querySelector('#tie > h1');
 
 /*----- event listeners -----*/ 
 document.getElementById('section').addEventListener('click', handleClick);
 
 /*----- functions -----*/
+init();
 
-function randomNum {
-    return Math.floor(Math.random() * 6) + 1;
+function handleClick() {
+    results.l = getRandomNums();
+    results.r = getRandomNums();
+    winner = getWinner();
+    scores[winner]++;
+    render();
 }
+
+function getWinner() {
+    return resultsp[l] === results.r ?
+    't'
+    : 
+    dice[results.l].points > dice[results.r] ? 'l' : 'r';
+}
+
+
+function getRandomNums() {
+    return dice.Math.floor(Math.random() * 6) + 1;
+}
+console.log(randomNum);
+
+function render() {
+    // render scores
+    lScoreEl.textContent = scores.l;
+    rScoreEl.textContent = scores.r;
+    tieScoreEl.textContent = scores.t;
+    // render results 
+    // lResultEl.style.backgroundImage = `url(${rps[results.l].img})`;
+    // rResultEl.style.backgroundImage = `url(${rps[results.r].img})`;
+    
+}
+
+function init() {
+    winner = null;
+    results = {
+        l: ''
+    }
+}
+
+
+
+
 
 function handleClick(evt) {
     on.click()
